be040036d7900feff978f41138736eedbbc44065
trevornei <117675657+trevornei@users.noreply.github.com>
2025-04-09 11:35:00 -0600

    Added a configuration file for firebase. This is the client side configuration.

---
+++ 1-fast-but-not-good-setup/start/config.js
@@ -0,0 +1,12 @@
+// Client Keys, okay to upload without .env masking.
+export const config = {
+    firebase: {
+        apiKey: "AIzaSyDodt1meqVP3tAp8-tYJWgKJ8nNlILu3OA",
+        authDomain: "markdown-669ef.firebaseapp.com",
+        projectId: "markdown-669ef",
+        storageBucket: "markdown-669ef.firebasestorage.app",
+        messagingSenderId: "995699706106",
+        appId: "1:995699706106:web:3f3a408ae22a66a66ca143",
+        measurementId: "G-1DK8W5Z06G"
+    }
+}
