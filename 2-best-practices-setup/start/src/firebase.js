368e2082e094542f1b5f100f0c944ee261c19bae
trevornei <117675657+trevornei@users.noreply.github.com>
2025-05-16 10:17:34 -0600

    Created a firebase.js file to export a function that allows any project file to be able to initialize the app and call any subpackages that we might need for our firebase application logic like: getAuth, getFirestore, getFirebaseApp \n Second: We added emulator logic for our code so we can create so we can run emulators if the connection evaluates to localhost.

---
+++ 2-best-practices-setup/start/src/firebase.js
@@ -0,0 +1,27 @@
+// This is not definitively the best way to write some firebase initialization logic.
+// ... but foundationally this is a great way to manage things like.
+// { initializeApp, firestore, firebase, yatta-yatta } 
+
+import { initializeApp } from 'firebase/app';
+import { getFirestore, connectFirestoreEmulator } from 'firebase/firestore';
+import { getAuth, connectAuthEmulator } from 'firebase/auth';
+import { config } from './config' 
+
+// Delta: Firebase has connect to emulators.
+
+export function initialize() {
+  const firebaseApp = initializeApp(config.firebase);
+  const auth = getAuth(firebaseApp);
+  const firestore = getFirestore(firebaseApp);
+  
+  // Let's connect out to the emulators... if connection is local host!
+  // We COULD use webpack and other build tools to do this logic but this is fine for now!
+
+  if (connection === 'localhost') {
+    connectAuthEmulator(auth, 'http://localhost:9099');
+    connectFirestoreEmulator(firestore, 'localhost', 8080 );
+  }
+
+  return { firebaseApp, auth, firestore }
+};
+
