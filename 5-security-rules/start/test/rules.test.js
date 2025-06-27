da77b3d7d919b568cb2e9e5b72749005ef1e97d2
trevornei <117675657+trevornei@users.noreply.github.com>
2025-06-27 12:49:22 -0600

    Catching up commits: Finished 5-security-rules

--- 5-security-rules/start/test/rules.test.js
+++ 5-security-rules/start/test/rules.test.js
@@ -22,18 +22,27 @@ function assertPermissionDenied(t, result) {
   t.is(result.code, 'permission-denied');
 }
 
+/*
 // 1
 test('An unauthenticated user fails to write to a profile', async (t) => {
-  const context = testEnv.unauthenticatedContext();
+  const context = testEnv.unauthenticatedContext('david_123');
   const userDoc = context.firestore().doc('users/david_123');
-  const result = await assertFails(userDoc.set({ name: 'Im david', email: 'blah@email.com' }));
+  const result = await assertSucceeds(userDoc.set({ name: 'Im david', email: 'blah@email.com' }));
   assertPermissionDenied(t, result);
 });
 
 // 2
 test('An authenticated user can write their profile', async (t) => {
-
+  // This time we will check what happens when the user is authenticated!!
+  const context = testEnv.authenticatedContext('david_123');
+  const userDoc = context.firestore().doc('users/david_123');
+  // Test if the assertion succeeds!
+  const result = await assertSucceeds(userDoc.set({ name: 'Im david', email: 'blah@email.com' }));
+  t.is(result, undefined);
 });
+
+
+
 
 // 3
 test('An unauthenticated user fails to write to expenses', async (t) => {
@@ -44,15 +53,23 @@ test('An unauthenticated user fails to write to expenses', async (t) => {
 test('An authenticated user can write to expenses', async (t) => {
 
 });
+*/
 
 // 4
 test('A user must have a first name and email', async (t) => {
-
+  // first name and email from the user object...
+  const context = testEnv.authenticatedContext('david_123');
+  const userDoc = context.firestore().doc('users/david_123');
+  const result = await assertSucceeds(userDoc.set({ name: 'Im david', email: 'blah@email.com' }));
+  t.is(result, undefined);
 });
 
 // 5
 test('An expense must have a cost greater than 0', async (t) => {
-
+  const context = testEnv.authenticatedContext('david_123');
+  const expenseDoc = context.firestore().doc('expense/some_expense');
+  const result = await assertFails(expenseDoc.set({cost: -11}));
+  assertPermissionDenied(t, result); 
 });
 
 // 6
